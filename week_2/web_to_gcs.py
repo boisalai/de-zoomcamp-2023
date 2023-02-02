e77a91a9fff8f0a5bcff900d346c7c49d5a0be19
Alain Boisvert <ay.boisvert@gmail.com>
2023-02-02 11:04:38 -0500

    commit Jeu  2 fév 2023 11:04:38 EST

--- week_2/web_to_gcs.py
+++ week_2/web_to_gcs.py
@@ -45,7 +45,7 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
-    print("This code comes from my github...")
+    print("This code comes from my github repo...")
     return df
 
 
